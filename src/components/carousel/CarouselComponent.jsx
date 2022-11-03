9a381ee7f45723bb159fd307f38a1ce07553053e
Andre Koga <social@koga.one>
2022-11-03 15:32:26 -0400

    added more images!

--- src/components/carousel/CarouselComponent.jsx
+++ src/components/carousel/CarouselComponent.jsx
@@ -20,6 +20,61 @@ export const CarouselComponent = () => {
       <div className="gradient">
         <Slider {...settings}>
           <Slide>
+            <StyledImage src="img/carousel/bdbi1.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi14.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi3.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi18.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi4.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi6.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi7.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi20.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi8.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi10.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi5.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi11.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi2.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi19.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi13.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi16.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi17.jpg" />
+          </Slide>
+          <Slide>
+            <StyledImage src="img/carousel/bdbi22.jpg" />
+          </Slide>
+          {/*  */}
+          <Slide>
             <StyledImage src="img/carousel/carousel1.jpg" />
           </Slide>
           <Slide>
