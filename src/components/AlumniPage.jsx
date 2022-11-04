ea6f17cd3b35fbf10e852503031b955a8d6a35d7
Moungsung <mslim1210@gmail.com>
2022-11-04 13:19:27 -0400

    Alumni Page
    Implemented the alumni page that looks like the leadership page. However as there weren't links for instagram and twitter, only their linked in links are shown.  In order to set the icon at center, I added two css rules for alumni page: 'social-link'. Also added their images from their linkedin.

--- src/components/AlumniPage.jsx
+++ src/components/AlumniPage.jsx
@@ -35,49 +35,52 @@ export const AlumniPage = (props) => {
 
   return (
     <div>
-    <div id='alumni' className='text-center'>
-      <Navigation />
-      <div className='col-md-8 col-md-offset-2 section-title'>
+      <div id='alumni' className='text-center'>
+        <Navigation />
+        <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Alumni</h2>
           <p>
             Check out our alumni from past years!
           </p>
-      </div>
-      {/* <div className="container">
-        <div className="btn-group">
-          {
-            years.map(num => (
-              <YearButton year={num}/>
-            ))
-          }
         </div>
-        <h1 className="year" style={{"margin": "1em 0em 1em 0em"}}>
-          {year}
-        </h1>
-
-        <div id='row' className="cardrow">
-            {JsonData.Alumni[year]
-            ? JsonData.Alumni[year].map((d, i) => (
-              <div className='card col-xs-4'>
-                <img src={d.img} alt=""/>
-                <div className='caption'>
-                    <h4>{d.name}</h4>
-                <div className="social-links">
-                  {d.linkedin ? <a href={d.linkedin} className="linkedin"><i class="fa fa-linkedin"></i></a> : ""}
-                  {d.instagram ? <a href={d.instagram} className="insta"><i class="fa fa-instagram"></i></a> : ""}
-                  {d.twitter ? <a href={d.twitter} className="twitter"><i class="fa fa-twitter"></i></a> : ""}
-                </div>
-                </div>
-              </div>
+        <div className="container">
+          {/* I'm not sure the year they were in BDBI */}
+          {/* <div className="btn-group">
+            {
+              years.map(num => (
+                <YearButton year={num} />
               ))
-          : 'hey'}
+            }
+          </div>
+          <h1 className="year" style={{ "margin": "1em 0em 1em 0em" }}>
+            {year}
+          </h1> */}
+
+          <div id='row' className="cardrow ">
+            {JsonData.Alumni
+              ? JsonData.Alumni.map((d, i) => (
+                <div className='card col-xs-4'>
+                  <img alt="" src={d.img} />
+                  <div className='caption'>
+                    <h4>{d.name}</h4>
+                    <p>{d.title}</p>
+                    <div class="social-link" style={{display:'inline'}}>
+                      {/* if linkin exists, then show the icon */}
+                      {d.linkedin ? <a href={d.linkedin} className="linkedin"><i class="fa fa-linkedin"></i></a> : ""}
+                      {/* {d.instagram ? <a href={d.instagram} className="insta"><i class="fa fa-instagram"></i></a> : ""} */}
+                      {/* {d.twitter ? <a href={d.twitter} className="twitter"><i class="fa fa-twitter"></i></a> : ""} */}
+                    </div>
+                  </div>
+                </div>
+              ))
+              : 'hey'}
+          </div>
         </div>
-      </div> */}
-        <h1 style={{'textAlign':'center','marginBottom':'2.76em'}}>COMING SOON</h1>
-      
-      
-    </div>
-    <Footer data={JsonData.Contact}/>
+        {/* <h1 style={{ 'textAlign': 'center', 'marginBottom': '2.76em' }}>COMING SOON</h1> */}
+
+
+      </div>
+      <Footer data={JsonData.Contact} />
     </div>
   )
 }
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,40 +4,40 @@ import JsonData from "../data/data.json";
 export const Team = (props) => {
   return (
     <div>
-    <div id='team' className='text-center'>
-      <Navigation />
-      <div className='col-md-8 col-md-offset-2 section-title'>
+      <div id='team' className='text-center'>
+        <Navigation />
+        <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Meet the Team</h2>
           <p>
             The people that make it all possible.
           </p>
         </div>
-      
+
         <div className="container">
-        <div id='row' className="cardrow ">
+          <div id='row' className="cardrow ">
             {JsonData.Team
-            ? JsonData.Team.map((d, i) => (
-              <div className='card col-xs-4'>
-                <img alt="" src={d.img} />
-                <div className='caption'>
+              ? JsonData.Team.map((d, i) => (
+                <div className='card col-xs-4'>
+                  <img alt="" src={d.img} />
+                  <div className='caption'>
                     <h4>{d.name}</h4>
                     <p>{d.title}</p>
-                <div class="social-links">
-                  {d.linkedin ? <a href={d.linkedin} className="linkedin"><i class="fa fa-linkedin"></i></a> : ""}
-                  {d.instagram ? <a href={d.instagram} className="insta"><i class="fa fa-instagram"></i></a> : ""}
-                  {d.twitter ? <a href={d.twitter} className="twitter"><i class="fa fa-twitter"></i></a> : ""}
+                    <div class="social-links">
+                      {d.linkedin ? <a href={d.linkedin} className="linkedin"><i class="fa fa-linkedin"></i></a> : ""}
+                      {d.instagram ? <a href={d.instagram} className="insta"><i class="fa fa-instagram"></i></a> : ""}
+                      {d.twitter ? <a href={d.twitter} className="twitter"><i class="fa fa-twitter"></i></a> : ""}
+                    </div>
+                  </div>
                 </div>
-                </div>
-              </div>
               ))
-            : 'hey'}
+              : 'hey'}
           </div>
-          </div>
-        
-      
-      
-    </div>
-    <Footer data={JsonData.Contact}/>
+        </div>
+
+
+
+      </div>
+      <Footer data={JsonData.Contact} />
     </div>
   )
 }
