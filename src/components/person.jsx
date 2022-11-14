e2dc0fb378197aee5d85caf5a90499ef75dfc2e7
Abhinav <abhinav.sattiraju@gmail.com>
2022-11-14 10:37:52 -0500

    past speakers updates and company logo slide bar updates

--- src/components/person.jsx
+++ src/components/person.jsx
@@ -10,20 +10,22 @@ export const Person = (props) => {
                 <h3>{props.name}</h3>
                 <span className="speakerText">{props.title}</span>
             </div>
-            <a
-                href={props.ld ? props.ld : "/"}
-                target="_blank"
-                rel="noreferrer"
-            >
-                <i className="fa fa-linkedin fa-2x"></i>
-            </a>
-            <a
-                href={props.tw ? props.tw : "/"}
-                target="_blank"
-                rel="noreferrer"
-            >
-                <i className="fa fa-twitter fa-2x"></i>
-            </a>
+            <div className="person_links">
+                <a
+                    href={props.ld ? props.ld : "/"}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i className="fa fa-linkedin fa-2x"></i>
+                </a>
+                <a
+                    href={props.tw ? props.tw : "/"}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i className="fa fa-twitter fa-2x"></i>
+                </a>
+            </div>
         </div>
     );
 };
